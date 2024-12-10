ae6f067f16725e730b0ce502a725e9fff2fbfeff
kollagreeshma <kollagreeshmasree2@gmail.com>
2024-12-10 10:12:06 +0530

    Add files via upload

---
+++ sample.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class sample{
+    public static void main(String args[]){
+        String a="HELLO";
+        String b=new String("HAI");
+        System.out.println(a);
+        System.out.println(b);
+    }
+
+}
