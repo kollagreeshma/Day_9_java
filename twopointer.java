cdada5c238e223889fe706b87cf6cd946e9bf3c5
kollagreeshma <kollagreeshmasree2@gmail.com>
2024-12-10 13:29:51 +0530

    Add files via upload

---
+++ twopointer.java
@@ -0,0 +1,20 @@
+/* two poniter technique reversing a string */
+import java.util.*;
+class twopointer{
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        String a=sc.nextLine();
+        char b[]=a.toCharArray();
+        int n=b.length;
+        int l=0;int h=n-1;
+        while(l<=h){
+        char temp=b[l];
+        b[l]=b[h];
+        b[h]=temp;
+        l++;
+        h--;
+        }
+        String res=new String(b);
+        System.out.println(res);
+    }
+}
