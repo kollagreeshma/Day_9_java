301b821e94bc1dddaca2bac4074609887458e54b
kollagreeshma <kollagreeshmasree2@gmail.com>
2024-12-10 13:28:09 +0530

    Add files via upload

---
+++ next.java
@@ -0,0 +1,11 @@
+/* using next or nextline */
+import java.util.*;
+class a{
+    public static void main(String args[]){
+        Scanner sc= new Scanner(System.in);
+        String a=sc.next();
+        System.out.println(a);
+        String b=sc.nextLine();
+        System.out.println(b);
+    }
+}
